f0cfbabf229c5f5c88d3786dd9b193df26d88ce6
Ben Simmons <ben.ryan.simmons@gmail.com>
2020-01-23 10:35:56 -0500

    Fix: Switch to locally hosted fonts

--- components/styles/GlobalStyles.tsx
+++ components/styles/GlobalStyles.tsx
@@ -1,14 +1,28 @@
 import { createGlobalStyle } from "styled-components";
-
+// import KumarOneOutline from "../../public/fonts/KumarOneOutline-Regular.ttf";
 const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css?family=Lato|Open+Sans|Oswald&display=swap');
+@import url('https://fonts.googleapis.com/css?family=Kumar+One+Outline|Roboto&display=swap');
+@font-face {
+    font-family: 'Kumar One Outline';
+    src: url('/fonts/Kumar_One_Outline/KumarOneOutline-Regular.ttf');
+    font-weight: normal;
+    font-style: normal;
+    font-display: auto;
+  }
+  @font-face {
+    font-family: 'Roboto';
+    src: url('/fonts/Roboto/Roboto-Regular.ttf');
+    font-weight: normal;
+    font-style: normal;
+    font-display: auto;
+
 html {
     box-sizing: border-box;
   }
   *, *:before, *:after {
     box-sizing: inherit;
     padding: 0;
-    margin: 0;
+    margin: 0; 
   }
   body {
     padding: 0;
@@ -17,7 +31,7 @@ html {
     background-repeat: no-repeat;
     background-size: 100vw 100vh;
     background-attachment: fixed;
-    font-family: 'Open Sans', Lato,Arial,Helvetica,sans-serif;
+    font-family: 'Roboto', Arial,Helvetica,sans-serif;
     font-size: 14px;
     line-height: 1.5
   }
