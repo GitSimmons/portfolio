2e4cd1967b52fe0ad6dcea8ca5b61323783977d8
Ben Simmons <ben.ryan.simmons@gmail.com>
2020-01-29 16:58:02 -0500

    Feat: Hero now displays a nice welcome message

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,58 +1,26 @@
 import styled from "styled-components";
 const StyledHero = styled.div`
+  display: flex;
+  align-items: center;
   background-image: radial-gradient(circle at left, #fff, #ddd);
-  padding: 1rem;
+  padding: 2rem;
   margin-bottom: 1rem;
-  height: 30rem;
-  header {
-    display: flex;
-    align-items: center;
-    margin-bottom: 0.62em;
-    width: 100%;
-    position: relative;
-  }
-  h3 {
-    display: block;
-    margin: 0;
-    font-size: 3.5rem;
-    /* text-transform: uppercase; */
-    font-family: "Kumar One Outline", cursive;
-  }
-  header:after {
-    content: "";
-    flex: auto;
-    display: inline-block;
-    height: 0.1rem;
-    margin-left: 1rem;
-    background-color: #000;
-  }
-  a {
-    text-decoration: none;
-    color: #be5657;
-    &:hover {
-      text-decoration: underline;
-    }
-  }
-  ul {
-    text-transform: uppercase;
-    list-style-type: none;
-    li {
-      display: inline-block;
-      &:not(:last-child) {
-        &:after {
-          content: "/";
-          margin: 0 0.5rem;
-        }
-      }
-    }
-  }
-  .views {
-    color: #be5657;
-  }
-  .tools {
-    color: #777;
+
+  span {
+    /* width: 100%; */
+    font-weight: 900;
+    font-size: 5vw;
   }
 `;
 
-const Hero = () => <StyledHero></StyledHero>;
+const Hero = () => (
+  <StyledHero>
+    <span>
+      Hi there, I'm Ben <br />
+      a Full Stack Developer
+      <br />
+      from Montreal, QC
+    </span>
+  </StyledHero>
+);
 export default Hero;
