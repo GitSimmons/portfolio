2b2120c493e29240c1d136bf0c6599253beb8cfc
Ben Simmons <ben.ryan.simmons@gmail.com>
2020-02-01 10:13:09 -0500

    refactor: locales to their own file'

---
+++ data/locales.ts
@@ -0,0 +1,16 @@
+export enum Locale {
+  en = "en",
+  fr = "fr"
+}
+export const locales: Locale[] = [Locale.en, Locale.fr];
+export const isLocale = (tested: string): tested is Locale =>
+  locales.some(locale => locale === tested);
+
+export const getInitialLocale = (): Locale => {
+  // get the language setting of the browser
+  const [browserSetting] = navigator.language.split("-");
+  if (isLocale(browserSetting)) {
+    return browserSetting;
+  }
+  return Locale.en;
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import { useRouter } from "next/dist/client/router";
-
-enum Locale {
-  en = "en",
-  fr = "fr"
-}
-const locales: Locale[] = [Locale.en, Locale.fr];
-const isLocale = (tested: string): tested is Locale =>
-  locales.some(locale => locale === tested);
-
-export function getInitialLocale(): Locale {
-  // the language setting of the browser
-  const [browserSetting] = navigator.language.split("-");
-  if (isLocale(browserSetting)) {
-    return browserSetting;
-  }
-  return Locale.en;
-}
+import { getInitialLocale } from "../data/locales";
 
 const Index: React.FC = () => {
   const router = useRouter();
